a068285ed06d113ce65bd9260416c1256270e5c2
bhoomikaaa20 <bhoomikasomavarapu15@gmail.com>
2026-03-29 07:40:01 +0530

    add:index file

--- server/index.ts
+++ server/index.ts
@@ -43,9 +43,8 @@ app.get("/api/test", (req, res) => {
 });
 
 // Server start
-const PORT: number = Number(process.env.PORT) || 5000;
+const PORT = parseInt(process.env.PORT || "5000", 10);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} `);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
